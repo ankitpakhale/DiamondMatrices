ad867f463cddf33baf8a3055f6ed0624e570412d
Ankit Pakhale <akp3067@gmail.com>
2023-04-20 10:28:00 +0530

    Minor changes

--- 01.py
+++ 01.py
@@ -5,7 +5,6 @@ path = "./Production Data/testing/imageDetails.py"
 with open(path, 'a') as file:
     file.write("\n# This is the text for testing")
 
-# os.system('ping 192.168.29.228')
 os.system('git add .')
 print("git added")
 os.system('git commit -m "Minor changes"')
