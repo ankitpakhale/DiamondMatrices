13bc013bc1202028937d388b97f6c0b6cfb05288
AnkitPakhale <akp3067@gmail.com>
2023-04-20 19:35:17 +0530

    Added: Minor Changes

--- AutoUpdatePush.py
+++ AutoUpdatePush.py
@@ -1,9 +1,13 @@
 import os
 
-path = "./Production Data/testing/dummy.py"
+path = "./Production Data/testing/imageDetails.py"
 
-with open("path", 'w') as file:
-    pass
- 
-    
-    
+with open(path, 'a') as file:
+    file.write("\n# This is the text for testing")
+
+os.system('git add .')
+print("******************************************** 1. git added ********************************************")
+os.system('git commit -m "Added: Minor Changes"')
+print("******************************************** 2. git commited ********************************************")
+os.system('git push')
+print("******************************************** 3. git pushed ********************************************")
